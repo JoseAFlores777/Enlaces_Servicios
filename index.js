812766ae1269890af00bd8e0168ae6bd17ef3b2f
JoseAFlores777 <65737946+JoseAFlores777@users.noreply.github.com>
2021-02-16 01:49:01 -0600

    Actualizacion

---
+++ index.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    ('use strict;')
+
+    let servicios = [{ nombre: "Servicio de Oracion", hora: "6:00 PM" },
+                     { nombre: "Conexión Juvenil", hora: "5:00 PM" },
+                     { nombre: "Servicio Dominical", hora: "9:00 AM" },
+                     { nombre: "Escuela Dominical", hora: "11:00 AM" },
+                     { nombre: "Servicio Evangelístico", hora: "5:00 PM" },
+                     { nombre: "Club Bíblico", hora: "0:00 PM" }
+    ];
+
+    var codServicio = 0,
+        url_f_iglesia = "",
+        url_f_conexion = "",
+        url_f_Youtube = "",
+        url_f_Zoom = "",
+        url_img = "img.jpeg";
+    
+        
+        $("#url_f_iglesia").css("visibility","visible");
+        $("#url_f_conexion").css("visibility","visible");
+        $("#url_f_Youtube").css("visibility","visible");
+        $("#url_f_Zoom").css("visibility","visible");
+    
+
+        $("#img").attr("src", url_img);
+
+        $("#servicio").text(servicios[codServicio]['nombre']);
+        $("#Versiculo").append(servicios[codServicio]['hora']);
+        
+        $("#url_f_iglesia").attr("href", url_f_iglesia);
+        $("#url_f_conexion").attr("href", url_f_conexion);
+        $("#url_f_Youtube").attr("href", url_f_Youtube);
+        $("#url_f_Zoom").attr("href", url_f_Zoom);
+    
+
+
+})
